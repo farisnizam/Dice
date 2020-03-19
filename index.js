08b20f781f11fd84de8550b0044e52e88b90971c
faris nizam <farisnizam001@gmail.com>
2020-03-19 15:44:05 +0800

    minor update

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
-// generate random number for dice 1 and dice 2
-var randomNumber1 = Math.floor(Math.random() * 6 + 1); // 1-6
-var randomNumber2 = Math.floor(Math.random() * 6 + 1); // 1-6
+// add event listener to play button
+document.querySelector(".playBtn").addEventListener("click", function () {
+  findWinner();
+});
 
-// change player 1 dice
-var randomDiceImage1 = "dice" + randomNumber1;
-var randomImageSource1 = "images/" + randomDiceImage1 + ".png";
-document.querySelector("img.img1").setAttribute("src", randomImageSource1);
+// detect keyboard press
+document.addEventListener("keypress", function (event) {
+  switch (event.key) {
 
-// change player 2 dice
-var randomDiceImage2 = "dice" + randomNumber2;
-var randomImageSource2 = "images/" + randomDiceImage2 + ".png";
-document.querySelector("img.img2").setAttribute("src", randomImageSource2);
+    case "Enter":
+      findWinner();
+      break;
+      
+    default:
+      break;
 
-// check who wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
-} else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
-} else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  }
+});
+
+
+function findWinner () {
+  // generate random number for dice 1 and dice 2
+  var randomNumber1 = Math.floor(Math.random() * 6 + 1); // 1-6
+  var randomNumber2 = Math.floor(Math.random() * 6 + 1); // 1-6
+
+  // change player 1 dice
+  var randomDiceImage1 = "dice" + randomNumber1;
+  var randomImageSource1 = "images/" + randomDiceImage1 + ".png";
+  document.querySelector("img.img1").setAttribute("src", randomImageSource1);
+
+  // change player 2 dice
+  var randomDiceImage2 = "dice" + randomNumber2;
+  var randomImageSource2 = "images/" + randomDiceImage2 + ".png";
+  document.querySelector("img.img2").setAttribute("src", randomImageSource2);
+
+  // check who wins
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 }
+
+
